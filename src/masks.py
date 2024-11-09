ef3a23d913306c4f7ed0567634fc2865abc7e595
Anna kvashnina <inttgroup@yandex.ru>
2024-11-09 23:29:23 +0700

    версия проекта с двумя функциями

---
+++ src/masks.py
@@ -0,0 +1,32 @@
+# def get_mask_card_number(card_number: str) -> str | None:
+#    """Bank card number masking function"""
+#    if card_number.isdigit() and len(card_number) == 16:
+#        return f"{card_number[:5]}{card_number[4:6]}{'*' * 2}{'*' * 4}{card_number[12:]}"
+#    else:
+#        return None
+
+
+# def get_mask_account(account_number: str) -> str | None:
+#    """Bank account number masking function"""
+#    if account_number.isdigit() and len(account_number) == 20:
+#        return f"{'*' * 2}{account_number[-4:]}"
+#    else:
+#        return None
+class ValidationError(Exception):
+    pass
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Bank card number masking function"""
+    if not card_number.isdigit() or len(card_number) != 16:
+        raise ValidationError("Invalid card number")
+
+    return f"{card_number[:5]}{card_number[4:6]}{'*' * 2}{'*' * 4}{card_number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Bank account number masking function"""
+    if not account_number.isdigit() or len(account_number) != 20:
+        raise ValidationError("Invalid account number")
+
+    return "**" + account_number[-4:]
